7c929b17f9344c7f2ee26d7ae3eed8a47024a626
saqlain noorani <saqlainnoorani17@gmail.com>
2025-08-18 14:30:28 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/admin/"],
     },
-    sitemap: "https://converthub.com/sitemap.xml",
+    sitemap: "https://playh5games.site/sitemap.xml",
   }
 }
